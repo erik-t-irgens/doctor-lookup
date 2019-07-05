8b067e25012e2096ce9f55e973383d68c2c9f28d
Erik Irgens <erik.t.irgens@gmail.com>
2019-07-05 16:32:20 -0700

    Finalize functionality, and finalize stylings.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
+
+
+
+setTimeout(function(){
+
+  $("#introCard").slideDown(2000);
+  $("#userForm").show();
+
+  setTimeout(function(){
+    $("#blankFiller").slideUp(2000);
+    $("#formCard").slideDown(2000);
+
+  }, 5000);
+}, 2000);
+
+setInterval(function(){
+  $("#resultLimitUpdate").text("Number of results: " + parseInt($("#resultLimit").val()))
+}, 50);
+
+
+
+
   $("#userForm").submit(function(event){
 
     let doctorName = $("#doctorName").val();
@@ -15,6 +37,8 @@ $(document).ready(function() {
     let doctorMiddleName;
     let doctorLastName;
     let doctorSearchName;
+
+// if function to determine whether or not user input more than 1 name.
 
     if (doctorName.includes(" ")  === true){
       if (doctorName.split(" ").length === 2) {
@@ -41,6 +65,8 @@ $(document).ready(function() {
     let symptoms;
     let symptomsSearch;
 
+// Counts if user input multiple symptoms!
+
     if (symptomsString.includes(" ") === true){
       symptoms = symptomsString.split(" ");
       for (var i = 0; i < symptoms.length; i++) {
@@ -62,13 +88,14 @@ console.log(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorSearch
 
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorSearchName}&query=${symptomsSearch}&gender=${doctorGender}&sort=${sorter}&skip=0&limit=${resultLimit}&user_key=${process.env.exports.apiKey}`).then(function(response){
 
-
       if (response.data.length === 0){
         $("#resultArea").html("<div class='alert alert-warning' role='alert'>Search provided no results. Try changing your search criteria! (Change the name, or your symptom!)</div>").slideDown(2000);
       } else if (response.data.length > 0){
         $("#resultArea").append("<div class='alert alert-success' role='alert'>Below are your results</div>");
         for (var i = 0; i < response.data.length; i++) {
-          $("#resultArea").append("<div class='card bg-info resultantCard'><div class='card-header'id='" + "cardNumber"+ i + "'<h3>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</h3></div><div class='card-body' id='" + "cardBody" + i + "'><img src='" + response.data[i].profile.image_url + "' alt='A picture of " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "'><p>Bio: " + response.data[i].profile.bio + "</p></div><hr>");
+          $("#resultArea").append("<div class='card bg-dark border-success resultantCard'><div class='card-header'id='" + "cardNumber"+ i + "'<h3>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</h3><br></div><div class='card-body' id='" + "cardBody" + i + "'><img src='" + response.data[i].profile.image_url + "' alt='A picture of " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "'><hr><p>Bio: " + response.data[i].profile.bio + "</p></div><hr>");
+
+
 
           if (response.data[i].practices.length > 0){
             if (response.data[i].practices[0].phones.length > 0){
@@ -88,12 +115,9 @@ console.log(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorSearch
             if (response.data[i].practices[0].accepts_new_patients === true) {
               $("#cardBody" + i).append(`<p class="emphatic">This doctor is currently accepting new patients.</p>`);
             } else if (response.data[i].practices[0].accepts_new_patients === false) {
-              $("#cardBody" + i).append(`<p>This doctor is currently NOT accepting new patients.</p>`);
+              $("#cardBody" + i).append(`<p class="emphaticBad">This doctor is currently NOT accepting new patients.</p>`);
             }
           }
-
-          $("#cardBody" + i).hide();
-          $("#cardBody" + i).slideDown(2000);
         }
         $("#resultArea").slideDown(2000);
       }
@@ -111,4 +135,6 @@ console.log(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorSearch
     event.preventDefault();
 
   });
+
+
 });
